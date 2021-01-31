0d063f2dad27863e9910bfd3c6b4a5b05b90258b
TAiki <b1016164@gmail.com>
2021-01-31 22:50:52 +0900

    Revert "Vue Installt途中"
    This reverts commit 0bb0f14cb796ff058610bdee30d80f5464e8b0e2.

--- laravel/resources/js/app.js
+++ laravel/resources/js/app.js
@@ -1,10 +1,1 @@
-import './bootstrap'
-import Vue from 'vue'
-import FooBar from './components/FooBar'
-
-const app = new Vue({
-  el: '#app',
-  components: {
-    FooBar,
-  }
-})
+require('./bootstrap');
--- laravel/resources/js/components/FooBar.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld msg="Hello World!"/>
-  </div>
-</template>
